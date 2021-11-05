51b7c754decee3a570bfb53944596a42a32e6813
wael bannani <79256781+waelbannani22@users.noreply.github.com>
2021-11-05 16:00:20 +0100

    commit

---
+++ volunty/volunty/Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  volunty
+//
+//  Created by wael bannani on 5/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var welcome: UILabel!
+    
+    @IBOutlet weak var rectangle1: UIImageView!
+}
+
