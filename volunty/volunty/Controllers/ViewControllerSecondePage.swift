bb67b77d476cf4e7f163bf1b6c5da6d2dd537666
rami96rihani <rami.rihani@esprit.tn>
2021-11-05 17:37:20 +0100

    Seconde page

---
+++ volunty/volunty/Controllers/ViewControllerSecondePage.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerSecondePage.swift
+//  volunty
+//
+//  Created by Mac2021 on 5/11/2021.
+//
+
+import UIKit
+
+class ViewControllerSecondePage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var ImgRectangle: UIImageView!
+    
+
+    @IBOutlet weak var ImgBg: UIImageView!
+    @IBOutlet weak var LabelWelcome: UILabel!
+    
+    @IBOutlet weak var LabelDesc: UILabel!
+    
+    @IBOutlet weak var ImgBg2: UIImageView!
+    
+    
+    @IBAction func BtnNext(_ sender: Any) {
+    }
+    
+    @IBAction func BtnNextArrow(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var LabelNext: UILabel!
+    
+}
