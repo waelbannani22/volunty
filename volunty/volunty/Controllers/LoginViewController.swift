e8d0c3e30b78f494d676589268748fa43b3106f1
waelbannani22 <79256781+waelbannani22@users.noreply.github.com>
2021-11-05 17:53:57 +0100

    Revert "third"
    This reverts commit 106b68dbb92e769024635b829c3581c432cda593.

--- volunty/volunty/Controllers/LoginViewController.swift
+++ volunty/volunty/Controllers/LoginViewController.swift
@@ -15,16 +15,15 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var image: UIImageView!
-    
-    @IBOutlet weak var passP: UIImageView!
-    @IBOutlet weak var password: UILabel!
-    
-    @IBOutlet weak var loginButton: UIImageView!
-    @IBOutlet weak var emailP: UIImageView!
-    @IBOutlet weak var email: UILabel!
-    @IBOutlet weak var your: UILabel!
-    @IBOutlet weak var create: UILabel!
-    @IBOutlet weak var rectangleblanc: UIImageView!
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
