a96d8114b46c0681fcf1b3d1fb72d5f604e0e4e6
waelbannani22 <79256781+waelbannani22@users.noreply.github.com>
2021-11-08 23:04:50 +0100

    qq

---
+++ volunty/volunty/Controllers/EmergencyCaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EmergencyCaseViewController.swift
+//  volunty
+//
+//  Created by wael bannani on 8/11/2021.
+//
+
+import UIKit
+
+class EmergencyCaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ volunty/volunty/Controllers/PostingRecruiterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PostingRecruiterViewController.swift
+//  volunty
+//
+//  Created by wael bannani on 8/11/2021.
+//
+
+import UIKit
+
+class PostingRecruiterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
