500d95d4b4f05f337e30d1b0f43821ca6c0c3219
waelbannani22 <79256781+waelbannani22@users.noreply.github.com>
2021-11-10 00:05:38 +0100

    pas 1

---
+++ volunty/volunty/Controllers/DateSelectionViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DateSelectionViewController.swift
+//  volunty
+//
+//  Created by wael bannani on 9/11/2021.
+//
+
+import UIKit
+
+class DateSelectionViewController: UIViewController {
+
+    var selectedDate = ""
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        datePicker.maximumDate = Date()
+        
+        if selectedDate.count != 0{
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd/MM/yyyy"
+            let date = dateFormatter.date(from: selectedDate)
+            
+            //datePicker.date = date!
+            
+        }
+        
+    }
+    
+
+    @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func doneTapped(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/M/YYYY"
+        let somedateString = dateFormatter.string(from: datePicker.date)
+        NotificationCenter.default.post(name: Notification.Name("dateAdded"), object: somedateString)
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
---
+++ volunty/volunty/Controllers/VolunteerEditProfileViewController.swift
@@ -0,0 +1,43 @@
+//
+//  VolunteerEditProfileViewController.swift
+//  volunty
+//
+//  Created by wael bannani on 9/11/2021.
+//
+
+import UIKit
+
+class VolunteerEditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func DateTapped(_ sender: Any) {
+        performSegue(withIdentifier: "date", sender: nil)
+    }
+    @IBOutlet weak var dateButton: UIButton!
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //if segue.identifier == "location"{
+         //   let destination = segue.destination as! WalkthroughLocationVC
+       //     destination.isSignUp = true
+       // }
+        
+        if segue.identifier == "date"{
+            let destination = segue.destination as! DateSelectionViewController
+            destination.selectedDate = dateButton.currentTitle!
+        }
+        
+       // if segue.identifier == "error"{
+       //     let destination = segue.destination as! LoginMessage
+      //      destination.message = "All fields are required to be fllled out and an image needs to be selected!"
+       // }
+        
+    }
+   
+
+}
